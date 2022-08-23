ab640f0783fb5182b691d0cda88219f7a433d4f7
Rafet can Tosun <rtosun@k2m10s05.42kocaeli.com.tr>
2022-08-23 12:05:10 +0300

    after

--- module02/ex00/Fixed.h
+++ module02/ex00/Fixed.h
@@ -12,7 +12,7 @@ public:
 	Fixed();
 	~Fixed();
 	Fixed(const Fixed &copy);
-	Fixed &operator = (const Fixed &src);
+	Fixed &operator= (const Fixed &src);
 	int		getRawBits(void) const;
 	void	setRawBits(int const raw);
 };
