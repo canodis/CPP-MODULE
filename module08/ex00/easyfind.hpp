34099f2f6605ee0746fe1dd23588d79af192f949
Rafet can Tosun <rtosun@k2m10s01.42kocaeli.com.tr>
2022-09-13 16:00:49 +0300

    m8

---
+++ module08/ex00/easyfind.hpp
@@ -0,0 +1,27 @@
+#pragma once
+# include <iostream>
+# include <iterator>
+
+class NotFound : public std::exception
+{
+public:
+	const char *what() const throw() {
+		return ("value is not in the conteiner !");
+	}
+};
+
+
+
+template<class T>
+typename T::iterator	easyfind(T &conteiner, int a) {
+	typename T::iterator _iter = conteiner.begin();
+	while (_iter != conteiner.end())
+	{
+		if (*_iter == a)
+			break;
+		_iter++;
+	}
+	if (_iter == conteiner.end())
+		throw NotFound();
+	return (_iter);
+}
---
+++ module08/ex00/main.cpp
@@ -0,0 +1,31 @@
+#include "easyfind.hpp"
+#include <vector>
+#include <array>
+#include <list>
+int main()
+{
+	std::vector<int> _vec;
+	std::array<int ,5> _arr = {0,1,2,3,4};
+	std::list<int> _list;
+
+	_list.push_back(5);
+	_list.push_back(6);
+	_list.push_back(7);
+
+	_vec.push_back(3);
+	_vec.push_back(4);
+	_vec.push_back(5);
+	try
+	{
+		std::cout << *easyfind(_vec, 5) << std::endl;
+		std::cout << *easyfind(_arr, 3) << std::endl;
+		std::cout << *easyfind(_vec, 4) << std::endl;
+		std::cout << *easyfind(_list, 15) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	
+}
