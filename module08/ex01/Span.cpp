a78b36beebdcc97517f28b6f3c085c6f82c0222a
can tosun <canodis@pop-os.localdomain>
2022-09-18 02:36:58 +0300

    asd

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -1,5 +1,6 @@
 #include "Span.h"
 #include <math.h>
+#include <climits>
 
 Span::Span() {
 }
---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <stack>
+#include <vector>
+
+template <typename T>
+class MutantStack : public std::stack<T, std::vector<T> > {
+	public:
+		MutantStack() {};
+		MutantStack(MutantStack const & other):std::stack<T, std::vector<T>>(other) {};
+		~MutantStack() {
+			this->c.clear();
+		};
+
+		MutantStack & operator=(MutantStack const & obj) {
+			if (obj != *this) {
+				this->c = obj.c;
+			}
+			return *this;
+		};
+
+
+		typedef typename std::vector<T>::iterator iterator;
+
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
+};
